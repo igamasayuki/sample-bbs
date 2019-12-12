16b66d7a172042ec437a96fdce1ffa381aed40bb
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro.local>
2019-12-12 13:34:57 +0900

    いいね機能をAjax/jQueryにする形で整備しました

---
+++ src/main/resources/static/js/like.js
@@ -0,0 +1,23 @@
+$(function() {
+	$('button').on("click", function() {
+		var articleId = $(this).val();
+		// ajaxでコントローラにリクエストを送信
+		$.ajax({
+			type : "GET", // GET or POST
+			url : "/like", // 送信先のコントローラURL
+			data : {
+				articleId : articleId
+			}, // リクエストパラメータをJSON形式で送る
+			dataType : "json", // レスポンスをJSON形式で受け取る
+			async : true, // true:非同期(デフォルト), false:同期
+		}).done(function(data) {
+			// 通信が成功した場合に受け取るメッセージ
+			likeCount = data["likeCount"];
+			$("#likeCount" + articleId).text(likeCount)
+		}).fail(function(XMLHttpRequest, textStatus, errorThrown) {
+			// 検索失敗時には、その旨をダイアログ表示
+			alert("リクエスト時にエラーが発生しました：" + textStatus + ":\n" + errorThrown);
+		});
+	});
+
+});
