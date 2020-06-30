00dbb9e933b1af863af7cbcb9e6d816c99e7288b
伊賀将之 <igamasayuki@igamasayukinoMacBook-puro-2.local>
2020-06-30 09:34:24 +0900

    無駄な変数を排除しました

--- src/main/java/jp/co/runy/bbs/joined/repository/JoinedArticleRepository.java
+++ src/main/java/jp/co/runy/bbs/joined/repository/JoinedArticleRepository.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -27,35 +26,47 @@ public class JoinedArticleRepository {
 
 	@Autowired
 	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
 
 	/**
 	 * articlesとcommentsテーブルを結合したものからarticleリストを作成する.
 	 * articleオブジェクト内にはcommentリストを格納する。
 	 */
 	private static final ResultSetExtractor<List<JoinedArticle>> ARTICLE_RESULT_SET_EXTRACTOR = (rs) -> {
+		// 記事一覧が入るarticleListを生成
 		List<JoinedArticle> articleList = new LinkedList<JoinedArticle>();
 		List<JoinedComment> commentList = null;
+		
+		// 前の行の記事IDを退避しておく変数
 		long beforeArticleId = 0;
+		
 		while (rs.next()) {
+			// 現在検索されている記事IDを退避
 			int nowArticleId = rs.getInt("id");
+			
+			// 現在の記事IDと前の記事IDが違う場合はArticleオブジェクトを生成
 			if (nowArticleId != beforeArticleId) {
 				JoinedArticle article = new JoinedArticle();
 				article.setId(nowArticleId);
 				article.setName(rs.getString("name"));
 				article.setContent(rs.getString("content"));
+				// 空のコメントリストを作成しArticleオブジェクトにセットしておく
 				commentList = new ArrayList<JoinedComment>();
 				article.setCommentList(commentList);
+				// コメントがセットされていない状態のArticleオブジェクトをarticleListオブジェクトにadd
 				articleList.add(article);
 			}
+			
+			// 記事だけあってコメントがない場合はCommentオブジェクトは作らない
 			if (rs.getInt("com_id") != 0) {
 				JoinedComment comment = new JoinedComment();
 				comment.setId(rs.getLong("com_id"));
 				comment.setName(rs.getString("com_name"));
 				comment.setContent(rs.getString("com_content"));
+				// コメントをarticleオブジェクト内にセットされているcommentListに直接addしている(参照型なのでこのようなことができる)
 				commentList.add(comment);
 			}
+			
+			// 現在の記事IDを前の記事IDを入れる退避IDに格納
 			beforeArticleId = nowArticleId;
 		}
 		return articleList;
@@ -69,7 +80,7 @@ public class JoinedArticleRepository {
 	public List<JoinedArticle> findAll() {
 		String sql = "SELECT a.id, a.name, a.content, com.id com_id, com.name com_name, com.content com_content,com.article_id "
 				+ "FROM articles a LEFT JOIN comments com ON a.id = com.article_id ORDER BY a.id DESC, com.id;";
-		List<JoinedArticle> articleList = jdbcTemplate.query(sql, ARTICLE_RESULT_SET_EXTRACTOR);
+		List<JoinedArticle> articleList = namedParameterJdbcTemplate.query(sql, ARTICLE_RESULT_SET_EXTRACTOR);
 
 		return articleList;
 	}
