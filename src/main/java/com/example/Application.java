4ce849d51709ff40222dbeaf04475dd8febdfacc
igamasayuki <igamasayuki@gmail.com>
2025-06-18 08:17:12 +0900

    デプロイ設定を削除

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -6,14 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
 }
